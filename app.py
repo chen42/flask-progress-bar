3233ef4767482b0beea6dc05625b4a7c6441118c
Hao Chen <inbox@haochen.name>
2019-05-11 18:56:49 -0500

    all working now

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ import tempfile
 import random
 import string
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 
+# the index.html file contains a query form, which requests the progress page upon submission
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# the progress page stores the query in session a cookie, and generates a temp file name for storing results,   
+# the progress.html waits events from the dosearch page 
 @app.route('/progress')
 def progress():
     tf_path=tempfile.gettempdir()
@@ -20,34 +22,30 @@ def progress():
     session['query']=request.args.get('query')
     return render_template('progress.html')
 
+# the sosearch pages submits progress, and save results to the temp file. Upon completion, it closes the result file and redirects to the showresult page.  
 @app.route('/dosearch')
 def dosearch():
     query=session['query']
+    out=open(session['path']+"result", "w+")
     def generate(query):
         x = 0
-        #out=open(session['path']+"graph", "w+")
-        out=open("/tmp/00agraph", "w+")
         result=str()
         while x <= 100:
             # do some work on query
-            result+=str(x)+" "
+            result+=query+str(x)+" <br>"
             out.write(result)
+            if x==100:
+                out.close()
             yield "data:"+str(x)+"\n\n"
-            #yield {"data": str(x) + "\n\n", "result":result }
-            #yield "data:" + str(x) + "\n\n", "result:" + result + "\n\n"
             x = x + 20
-            time.sleep(0.2)
-        out.close()
+            time.sleep(1)
     return Response(generate(query), mimetype= 'text/event-stream')
 
-@app.route('/showgraph')
-def showgraph():
-    #with open (session['path']+"graph", "r") as f:
-    with open ("/tmp/00agraph", "r") as f:
-        graphdata=f.read()
-    print("there should be data here-->"+str(graphdata) + "<<<-")
-    #return render_template('cytoscape.html', graphdata=str(graphdata))
-    return render_template('cytoscape.html', results=str(graphdata))
+@app.route('/showresult')
+def showresult():
+    with open (session['path']+"result", "r") as f:
+        result=f.read()
+    return render_template('results.html', results=result)
 
 if __name__ == "__main__":
 	app.run(debug=True)
